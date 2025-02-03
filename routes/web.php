ead3fdf2ed1d6eb791ef290f98b977dd3ce21468
RakaMuttaqin <rakamuttaqin13@gmail.com>
2025-02-03 09:41:19 +0700

    menambahkan route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('cek-login');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::group(['middleware' => 'checkrole:Admin'], function () {
+        Route::get('/', function () {
+            return view('admin.dashboard');
+        })->name('dashboard');
+
+        Route::get('/daftar-pengguna', function () {
+            return view('admin.referensi.users.index');
+        })->name('daftar-pengguna');
+    });
 });
