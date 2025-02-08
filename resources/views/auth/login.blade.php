e053277c1b3604a7b03aa6d64f01a51fe037b093
RakaMuttaqin <rakamuttaqin13@gmail.com>
2025-02-08 12:10:58 +0700

    mengubah tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,89 +1,221 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="loading bordered-layout" lang="en" data-layout="bordered-layout" data-textdirection="ltr">
+<!-- BEGIN: Head-->
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="InvSmart adalah suatu sistem yang dibangun untuk mengatur dan mengelola data-data yang berhubungan dengan inventaris sekolah.">
+    <meta name="keywords" content="Inventory Sekolah, Inventory, Sekolah">
+    <meta name="author" content="RAKAMUTTAQIN">
+    <title>Login - InvSmart</title>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Inventaris Barang - Login</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('assets') }}/dist/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    <link rel="apple-touch-icon" href="{{ asset('app-assets') }}/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('app-assets') }}/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
         rel="stylesheet">
 
-    <!-- Custom styles for this template-->
-    <link href="{{ asset('assets') }}/dist/css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/vendors/css/vendors.min.css">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/css/components.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/css/themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/css/themes/bordered-layout.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/css/themes/semi-dark-layout.css">
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/css/plugins/forms/form-validation.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets') }}/css/pages/authentication.css">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/style.css">
+    <!-- END: Custom CSS-->
 
 </head>
+<!-- END: Head-->
 
-<body class="bg-gradient-primary">
+<!-- BEGIN: Body-->
 
-    <div class="container flex">
+<body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click"
+    data-menu="vertical-menu-modern" data-col="blank-page">
+    <!-- BEGIN: Content-->
+    <div class="app-content content ">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <div class="auth-wrapper auth-basic px-2">
+                    <div class="auth-inner my-2">
+                        <!-- Login basic -->
+                        <div class="card mb-0">
+                            <div class="card-body">
+                                <a href="index.html" class="brand-logo">
+                                    <svg viewbox="0 0 139 95" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink" height="28">
+                                        <defs>
+                                            <lineargradient id="linearGradient-1" x1="100%" y1="10.5120544%"
+                                                x2="50%" y2="89.4879456%">
+                                                <stop stop-color="#000000" offset="0%"></stop>
+                                                <stop stop-color="#FFFFFF" offset="100%"></stop>
+                                            </lineargradient>
+                                            <lineargradient id="linearGradient-2" x1="64.0437835%" y1="46.3276743%"
+                                                x2="37.373316%" y2="100%">
+                                                <stop stop-color="#EEEEEE" stop-opacity="0" offset="0%"></stop>
+                                                <stop stop-color="#FFFFFF" offset="100%"></stop>
+                                            </lineargradient>
+                                        </defs>
+                                        <g id="Page-1" stroke="none" stroke-width="1" fill="none"
+                                            fill-rule="evenodd">
+                                            <g id="Artboard" transform="translate(-400.000000, -178.000000)">
+                                                <g id="Group" transform="translate(400.000000, 178.000000)">
+                                                    <path class="text-primary" id="Path"
+                                                        d="M-5.68434189e-14,2.84217094e-14 L39.1816085,2.84217094e-14 L69.3453773,32.2519224 L101.428699,2.84217094e-14 L138.784583,2.84217094e-14 L138.784199,29.8015838 C137.958931,37.3510206 135.784352,42.5567762 132.260463,45.4188507 C128.736573,48.2809251 112.33867,64.5239941 83.0667527,94.1480575 L56.2750821,94.1480575 L6.71554594,44.4188507 C2.46876683,39.9813776 0.345377275,35.1089553 0.345377275,29.8015838 C0.345377275,24.4942122 0.230251516,14.560351 -5.68434189e-14,2.84217094e-14 Z"
+                                                        style="fill: currentColor"></path>
+                                                    <path id="Path1"
+                                                        d="M69.3453773,32.2519224 L101.428699,1.42108547e-14 L138.784583,1.42108547e-14 L138.784199,29.8015838 C137.958931,37.3510206 135.784352,42.5567762 132.260463,45.4188507 C128.736573,48.2809251 112.33867,64.5239941 83.0667527,94.1480575 L56.2750821,94.1480575 L32.8435758,70.5039241 L69.3453773,32.2519224 Z"
+                                                        fill="url(#linearGradient-1)" opacity="0.2"></path>
+                                                    <polygon id="Path-2" fill="#000000" opacity="0.049999997"
+                                                        points="69.3922914 32.4202615 32.8435758 70.5039241 54.0490008 16.1851325">
+                                                    </polygon>
+                                                    <polygon id="Path-21" fill="#000000" opacity="0.099999994"
+                                                        points="69.3922914 32.4202615 32.8435758 70.5039241 58.3683556 20.7402338">
+                                                    </polygon>
+                                                    <polygon id="Path-3" fill="url(#linearGradient-2)"
+                                                        opacity="0.099999994"
+                                                        points="101.428699 0 83.0667527 94.1480575 130.378721 47.0740288">
+                                                    </polygon>
+                                                </g>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                    <h2 class="brand-text text-primary ms-1">InvSmart</h2>
+                                </a>
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center alighn-center justify-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                <h4 class="card-title mb-1">Welcome to InvSmart! 👋</h4>
+                                <p class="card-text mb-2">Please sign-in to your account and start the adventure</p>
+
+                                <form class="auth-login-form mt-2 needs-validation" action="{{ route('cek-login') }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('POST')
+                                    <div class="mb-1">
+                                        <label for="user_nama" class="form-label">Username</label>
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" id="user_nama"
+                                                name="user_nama" placeholder="johndoe" aria-describedby="user_nama"
+                                                tabindex="1" autofocus required />
+                                        </div>
                                     </div>
-                                    <form class="user" method="POST" action="{{ route('cek-login') }}">
-                                        @csrf
-                                        @method('POST')
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" id="user_nama"
-                                                aria-describedby="emailHelp" placeholder="Enter User Nama..."
-                                                name="user_nama">
+
+                                    <div class="mb-1">
+                                        <label class="form-label" for="user_pass">Password</label>
+                                        <div class="input-group form-password-toggle">
+                                            <input type="password" class="form-control form-control-merge"
+                                                id="user_pass" name="user_pass" tabindex="2"
+                                                placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                                minlength="6" required />
+                                            <span class="input-group-text cursor-pointer"><i
+                                                    data-feather="eye"></i></span>
                                         </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="user_pass"
-                                                placeholder="Password" name="user_pass">
+                                    </div>
+                                    <div class="mb-1">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" id="remember-me"
+                                                tabindex="3" />
+                                            <label class="form-check-label" for="remember-me"> Remember Me </label>
                                         </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </button>
-                                    </form>
-                                </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary w-100" tabindex="4">
+                                        Sign in
+                                    </button>
+                                </form>
                             </div>
                         </div>
+                        <!-- /Login basic -->
                     </div>
                 </div>
 
             </div>
-
         </div>
-
     </div>
+    <!-- END: Content-->
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('assets') }}/dist/vendor/jquery/jquery.min.js"></script>
-    <script src="{{ asset('assets') }}/dist/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="{{ asset('assets') }}/dist/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- BEGIN: Vendor JS-->
+    <script src="{{ asset('app-assets') }}/vendors/js/vendors.min.js"></script>
+    <!-- BEGIN Vendor JS-->
 
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="{{ asset('app-assets') }}/vendors/js/forms/validation/jquery.validate.min.js"></script>
+    <!-- END: Page Vendor JS-->
 
+    <!-- BEGIN: Theme JS-->
+    <script src="{{ asset('app-assets') }}/js/core/app-menu.js"></script>
+    <script src="{{ asset('app-assets') }}/js/core/app.js"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    {{-- <script src="{{ asset('app-assets') }}/js/scripts/pages/auth-login.js"></script> --}}
+    <!-- END: Page JS-->
+
+    <script>
+        $(document).ready(function() {
+            $('.auth-login-form').validate({
+                rules: {
+                    user_nama: {
+                        required: true,
+                        minlength: 3
+                    },
+                    user_pass: {
+                        required: true,
+                        minlength: 6
+                    }
+                },
+                messages: {
+                    user_nama: {
+                        required: "Nama Pengguna harus diisi.",
+                        minlength: "Nama Pengguna minimal 3 karakter."
+                    },
+                    user_pass: {
+                        required: "Password harus diisi.",
+                        minlength: "Password minimal 6 karakter."
+                    }
+                },
+                errorPlacement: function(error, element) {
+                    error.addClass("invalid-feedback");
+                    element.closest(".mb-1").append(error);
+                    element.addClass("is-invalid");
+                },
+                highlight: function(element) {
+                    $(element).addClass("is-invalid");
+                },
+                unhighlight: function(element) {
+                    $(element).removeClass("is-invalid");
+                }
+            });
+        });
+
+
+        $(window).on('load', function() {
+            if (feather) {
+                feather.replace({
+                    width: 14,
+                    height: 14
+                });
+            }
+        })
+    </script>
 </body>
+<!-- END: Body-->
 
 </html>
